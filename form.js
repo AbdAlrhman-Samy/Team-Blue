610500f16190f50ff5a5117d5fe5a50ef9d4e462
abdalrhman-samy <abdalrhman.samy@outlook.com>
2020-11-13 04:42:22 +0200

    added rec form

---
+++ form.js
@@ -0,0 +1,53 @@
+const bigTest = document.getElementById("committeeQs");
+const one = document.getElementById("allCommitteeQs");
+const two = document.getElementById("sciCommitteeQs");
+const sciQ1 = document.getElementById("sciQ1");
+const sciQ2 = document.getElementById("sciQ2");
+const subBtn = document.getElementById("sub-btn");
+const all = document.getElementById("all");
+const teaser = document.getElementById("teaser");
+
+
+let today = new Date();
+let recDay = new Date(2020, 10, 12, 15, 0, 0)
+
+
+  if (today >= recDay) {
+    teaser.classList.add('d-none')
+    all.classList.remove('d-none')    
+    console.log(today);
+  };
+
+subBtn.hidden = true;
+
+const check = (bruh) => {
+  switch (bruh) {
+    case 0:
+      subBtn.hidden = false;
+      bigTest.classList.remove("d-none");
+      one.classList.remove("d-none");
+      two.classList.add("d-none");
+      sciQ1.required = false;
+      sciQ2.required = false;
+      break;
+
+    case 1:
+      subBtn.hidden = false;
+      bigTest.classList.remove("d-none");
+      one.classList.remove("d-none");
+      two.classList.remove("d-none");
+      sciQ1.required = true;
+      sciQ2.required = true;
+      break;
+
+    default:
+      subBtn.hidden = true;
+      bigTest.classList.add("d-none");
+      one.classList.add("d-none");
+      two.classList.add("d-none");
+      sciQ1.required = false;
+      sciQ2.required = false;
+      break;
+  }
+};
+
